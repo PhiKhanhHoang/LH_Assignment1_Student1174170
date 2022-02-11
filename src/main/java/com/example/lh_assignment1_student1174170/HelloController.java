a44150c5410fe5232891e76b3b3d103937c3f81b
Phi Khanh Hoang <94206447+PhiKhanhHoang@users.noreply.github.com>
2022-02-11 13:23:15 -0500

    Initial commit

---
+++ src/main/java/com/example/lh_assignment1_student1174170/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.lh_assignment1_student1174170;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.lh_assignment1_student1174170 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.lh_assignment1_student1174170 to javafx.fxml;
+    exports com.example.lh_assignment1_student1174170;
+}
